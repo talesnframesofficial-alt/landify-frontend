08d84ad50fbaa0950ec571d670415b5d12297b13
talesnframesofficial-alt <talesnframesofficial@gmail.com>
2025-11-22 17:54:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 import "../styles/globals.css";
 import { Navbar } from "../components/Navbar";
+import MobileNav from "../components/MobileNav";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="bg-white text-slate-800">
+        {/* TOP NAVBAR */}
         <Navbar />
-        <main className="pt-20 pb-10">{children}</main>
+
+        {/* MAIN CONTENT AREA */}
+        {/* Added extra pb-24 so bottom nav doesn't cover content */}
+        <main className="pt-20 pb-24">{children}</main>
+
+        {/* MOBILE BOTTOM NAVBAR */}
+        <MobileNav />
       </body>
     </html>
   );
