79dabf15e29a085fb4947c6bb698ef2e00d87bc0
talesnframesofficial-alt <talesnframesofficial@gmail.com>
2025-11-25 13:57:50 +0530

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req: any) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  await supabase.auth.getSession();
+  return res;
+}
+
+export const config = {
+  matcher: ['/profile', '/post-ad', '/chat/:path*']
+};
